61078cff584f14bbce0f3ed1d87e7a53f2e0008a
Krzysztof Półchłopek <polchloek.krzysztof@gmail.com>
2022-06-05 08:42:49 +0200

    Files creation and CMake modifications

---
+++ BME280/cli/include/cli.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file cli.hpp
+ * @authors Krzysztof Półchłopek, Michał Tomacha
+ * @version 0.1
+ * @date 2022-05-20
+ * 
+ * @brief Library for handling communcation with PC
+ */
+
+#ifndef _CLI_HPP_
+#define _CLI_HPP_
+
+
+namespace CLI
+{
+    
+} // namespace CLI
+
+
+#endif
